c5e7d88c204863f5c81ba730eb5f294ff8c77d5c
Baseddragoon <ard14cool@gmail.com>
2024-08-13 18:06:51 -0400

    revision 2.03

--- app.py
+++ app.py
@@ -1,38 +1,35 @@
-#importing libraries
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-#load in the data
+# Load the data
 cars = pd.read_csv('vehicles_us.csv')
 
-#make a header
-st.header ('Car Data Analysis')
+# Display header
+st.header('Car Data Analysis')
 
 # Add a checkbox to filter data
 filter_cars = st.checkbox('Show only SUVs')
 
- # Add a checkbox to filter data
-
-filter_cars = st.checkbox('Show only SUVs')
 if filter_cars:
+    # Filter the DataFrame to show only SUVs
     filtered_cars = cars[cars['type'] == 'SUV']
 else:
+    # Show the original DataFrame if checkbox is not checked
     filtered_cars = cars
 
-#creating a histogram
-fig_hist = px.histogram(cars, x='type', color='paint_color',  title='How common colors are on vehicle types')
+# Creating a histogram
+fig_hist = px.histogram(filtered_cars, x='type', color='paint_color', title='How common colors are on vehicle types')
 st.plotly_chart(fig_hist)
 
-#creating a histogram
-fig_hist2 = px.histogram(cars, x='model',  color='cylinders',title='Cylinder Type on vehicle brands')
+# Creating a second histogram
+fig_hist2 = px.histogram(filtered_cars, x='model', color='cylinders', title='Cylinder Type on vehicle brands')
 st.plotly_chart(fig_hist2)
 
-#creating a scatterplot
+# Creating a scatterplot
 fig_scatter = px.scatter(filtered_cars, x='model_year', y='odometer', color='model', title='Odometer of vehicles')
 st.plotly_chart(fig_scatter)
 
-
- # Add a checkbox to show scatter plot
+# Add a checkbox to show scatter plot
 if st.checkbox('Show Scatter Plot'):
     st.plotly_chart(fig_scatter)
