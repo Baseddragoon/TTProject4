82dc2be5e606060993707823991cc4624093b93e
Baseddragoon <ard14cool@gmail.com>
2024-08-13 17:41:26 -0400

    revision 2.01

--- app.py
+++ app.py
@@ -9,6 +9,18 @@ cars = pd.read_csv('vehicles_us.csv')
 #make a header
 st.header ('Car Data Analysis')
 
+# Add a checkbox to filter data
+filter_cars = st.checkbox('Show only SUVs')
+
+ # Add a checkbox to filter data
+if st.checkbox('Show Scatter Plot'):
+    st.plotly_chart(fig_scatter)
+filter_cars = st.checkbox('Show only SUVs')
+if filter_cars:
+    filtered_cars = cars[cars['type'] == 'SUV']
+else:
+    filtered_cars = cars
+
 #creating a histogram
 fig_hist = px.histogram(cars, x='type', color='paint_color',  title='How common colors are on vehicle types')
 st.plotly_chart(fig_hist)
@@ -18,15 +30,10 @@ fig_hist2 = px.histogram(cars, x='model',  color='cylinders',title='Cylinder Typ
 st.plotly_chart(fig_hist2)
 
 #creating a scatterplot
-fig_scatter = px.scatter(cars, x='model_year', y='odometer', color='model', title='Odometer of vehicles')
+fig_scatter = px.scatter(filtered_cars, x='model_year', y='odometer', color='model', title='Odometer of vehicles')
 st.plotly_chart(fig_scatter)
 
-#add slider to show display
+
  # Add a checkbox to show scatter plot
 if st.checkbox('Show Scatter Plot'):
     st.plotly_chart(fig_scatter)
-filter_cars = st.checkbox('Show only Ford F150')
-if filter_cars:
-    filtered_cars = cars[cars['model'] == 'ford f150']
-else:
-    filtered_cars = cars
